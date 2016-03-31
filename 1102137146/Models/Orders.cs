cd230109654a2c5c53cb39b4fcfe73ca8ea2221c
1102137226 <1102137226@gm.kuas.edu.tw>
2016-03-31 15:56:42 +0800

    111

---
+++ 1102137146/Models/Orders.cs
@@ -0,0 +1,42 @@
+namespace _1102137146.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Sales.Orders")]
+    public partial class Orders
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Orders()
+        {
+            this.OrderDetails = new HashSet<OrderDetails>();
+        }
+
+        public int OrderID { get; set; }
+        public Nullable<int> CustomerID { get; set; }
+        public int EmployeeID { get; set; }
+        public string OrderDate { get; set; }
+        public string RequiredDate { get; set; }
+        public string ShippedDate { get; set; }
+        public int ShipperID { get; set; }
+        public decimal Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+        public string CompanyName { get; set; }
+        public string EmpName { get; set; }
+        public string ShipperName { get; set; }
+
+        public virtual Employees Employees { get; set; }
+        public virtual Customers Customers { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderDetails> OrderDetails { get; set; }
+        public virtual Shippers Shippers { get; set; }
+    }
+}
